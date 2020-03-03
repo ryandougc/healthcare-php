18391fdca7b018a9cd4f729eaeec728710cd6922
ryandougc <47313734+ryandougc@users.noreply.github.com>
2020-03-02 22:58:05 -0800

    cleaned index.php

--- index.php
+++ index.php
@@ -7,13 +7,5 @@ if(isset($_SESSION['signedin'])){
     header('location: auth/index.php');
 }
 
-<<<<<<< HEAD
-?>
-<p>hrello</p>
-=======
-
-
 ?>
 
-<p>d</p>
->>>>>>> 1e62b550dc75b7eb78a54b9f17258a8a9edefe21
