04f8742560d1a1ddb847c420a538b2baa191c453
ryandougc <ryan.carr@email.kpu.ca>
2020-03-08 23:30:48 -0700

    Table type changes

--- databasetables.sql
+++ databasetables.sql
@@ -1,29 +1,29 @@
 CREATE  TABLE CLINIC(
-    ClinicID            VarChar(35)     NOT NULL,
+    ClinicID            VarChar(36)     NOT NULL,
     ClinicAddress       VarChar(30)     NOT NULL,
     ClinicPhone         VarChar(12)     NOT NULL,
     CONSTRAINT          CLINIC_PK   PRIMARY KEY(ClinicID)
     );
 
 CREATE  TABLE ACCOUNT
-    AccountID           VarChar(25)     NOT NULL,
-    LoginID             VarChar(35)     NULL,
-    AccountPassword     VarChar(25)     NULL,
+    AccountID           VarChar(36)     NOT NULL,
+    LoginID             VarChar(36)     NULL,
+    AccountPassword     VarChar(255)     NULL,
     FirstName           VarChar(25)     NOT NULL,
     LastName            VarChar(25)     NOT NULL,
-    AccountType         VarChar(35)     NOT NULL,
+    AccountType         VarChar(36)     NOT NULL,
     CONSTRAINT          ACCOUNT_PK      PRIMARY KEY(AccountID)                      
     );
 
 CREATE  TABLE SYSTEM_ADMIN (
-    SysAdminID          VarChar(35)     NOT NULL,
+    SysAdminID          VarChar(36)     NOT NULL,
     CONSTRAINT          SYSADMIN_PK     PRIMARY KEY (SysAdminID)
         );
 
 CREATE  TABLE DOCTOR (
-    DoctorID            VarChar(35)     NOT NULL,
-    ClinicID            VarChar(35)     NOT NULL,
-    DoctorEmail         VarChar(35)     NOT NULL,
+    DoctorID            VarChar(36)     NOT NULL,
+    ClinicID            VarChar(36)     NOT NULL,
+    DoctorEmail         VarChar(36)     NOT NULL,
     CONSTRAINT          DOCTOR_PK       PRIMARY KEY (DoctorID),
     CONSTRAINT          DOC_CLINIC_FK   FOREIGN KEY (ClinicID)
                             REFERENCES CLINIC (ClinicID)
@@ -32,8 +32,8 @@ CREATE  TABLE DOCTOR (
     );
 
 CREATE  TABLE STAFF (
-    StaffID             VarChar(35)     NOT NULL,
-    ClinicID            VarChar(35)     NOT NULL,
+    StaffID             VarChar(36)     NOT NULL,
+    ClinicID            VarChar(36)     NOT NULL,
     CONSTRAINT          STAFF_PK        PRIMARY KEY (StaffID),
     CONSTRAINT          STAFF_CLINIC_FK FOREIGN KEY (ClinicID)
                             REFERENCES CLINIC (ClinicID)
@@ -42,32 +42,32 @@ CREATE  TABLE STAFF (
     );
 
 CREATE  TABLE PATIENT (
-    PatientID           VarChar(35)     NOT NULL,
-    PatientEmail        VarChar(35)     NOT NULL,
-    PatientPhone        VarChar(12)     NOT NULL,
-    PatientAddress      VarChar(35)     NOT NULL,
+    PatientID           VarChar(36)     NOT NULL,
+    PatientEmail        VarChar(36)     NOT NULL,
+    PatientPhone        VarChar(15)     NOT NULL,
+    PatientAddress      VarChar(36)     NOT NULL,
     EmailNotifications  Boolean         NOT NULL,
     CONSTRAINT          PATIENT_PK      PRIMARY KEY (PatientID)
     );
 
 CREATE  TABLE EXAM_RESULTS (
-    PatientID           VarChar(35)     NOT NULL,
-    DoctorID            VarChar(35)     NOT NULL,
-    ExamDate            Date            NOT NULL,
-    ExamSubject         VarChar(35)     NOT NULL,
-    ExamResults         VarChar(35)     NOT NULL,
+    PatientID           VarChar(36)     NOT NULL,
+    DoctorID            VarChar(36)     NOT NULL,
+    ExamDate            TIMESTAMP       NOT NULL,
+    ExamSubject         VarChar(100)     NOT NULL,
+    ExamResults         VarChar(255)     NOT NULL,
     CONSTRAINT          PAT_DOCTOR_PK   PRIMARY KEY (PatientID, DoctorID)
     );
 
 CREATE  TABLE VISIT (
-    VisitID             VarChar(35)     NOT NULL,
-    DoctorID            VarChar(35)     NOT NULL,
-    ClinicID            VarChar(35)     NOT NULL,
-    PatientID           VarChar(35)     NOT NULL,
-    VisitDate           Date            NOT NULL,
-    Prescription        VarChar(35)     NOT NULL,
-    DoctorNotes         VarChar(35)     NOT NULL,
-    SuggestedExam       VarChar(35)     NOT NULL,
+    VisitID             VarChar(36)     NOT NULL,
+    DoctorID            VarChar(36)     NOT NULL,
+    ClinicID            VarChar(36)     NOT NULL,
+    PatientID           VarChar(36)     NOT NULL,
+    VisitDate           TIMESTAMP       NOT NULL,
+    Prescription        VarChar(255)     NOT NULL,
+    DoctorNotes         VarChar(255)     NOT NULL,
+    SuggestedExam       VarChar(100)     NOT NULL,
     CONSTRAINT          VIST_PK         PRIMARY KEY (VisitID),
     CONSTRAINT          VISIT_CLINIC_FK FOREIGN KEY (ClinicID)
                             REFERENCES CLINIC (ClinicID)
