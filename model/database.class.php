d9a20d2d1bfff1e44cfcf4715afcef7251181cc8
ryandougc <ryan.carr@email.kpu.ca>
2020-03-08 23:31:24 -0700

    OOP database connection

---
+++ model/database.class.php
@@ -0,0 +1,19 @@
+<?php
+
+class Database {
+    private $host = "localhost";
+    private $user = "dev";
+    private $pwd = "healthcare";
+    private $dbName = "healthcare-system";
+
+    protected function connect() {
+        try{
+            $dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->dbName;
+            $pdo = new PDO($dsn, $this->user, $this->pwd);
+            $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+            return $pdo;
+        } catch(PDOException $e) {
+            die("ERROR. Could not connect to database: " . $e->getMessage());
+        }
+    }
+}
