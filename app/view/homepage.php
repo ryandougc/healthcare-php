fbbfac64b7e4d1d2c83480797efadf7b26fb7330
ryandougc <ryan.carr@email.kpu.ca>
2020-03-25 11:26:03 -0700

    Implement Access Control

--- app/view/homepage.php
+++ app/view/homepage.php
@@ -3,36 +3,39 @@
 
 <?php 
 
-require('../includes/patientAccess.php');
+//Check if a user is logged in. Send them to the signin page if they aren't
+require_once('../includes/sessionCheck.php');
+//Only give access to patients
+require_once('../includes/patientAccess.php');
 
+//include all of the controllers and models
 include '../model/database.class.php';
 include '../model/account.class.php';
 include '../model/patient.class.php';
 include '../controller/accountcontroller.php';
 include '../controller/patientcontroller.php';
 
-
+//Include the page header
 include '../view/partials/header.php'; 
 
-
+//If a use is signed in, 
 if(isset($_SESSION['signedin'])){
-    echo $_SESSION['loginid'];
     $patient = new PatientController();
     $patientDetails = $patient->getPatientProfile($_SESSION['loginid']);
 }
 
+//Log the user out on click
 if(isset($_GET['action']) && $_GET['action'] != ""){
     $account = new AccountController();
     $account->logout();
 }
-
 ?>
 
 <a href="?action=signout">Sign Out</a>
 
 <h1>Welcome <?php echo $_SESSION['firstName'] . " " . $_SESSION['lastName']; ?></h1>
 
-<a href="">View your profile</a>
+<a href="editPatientProfile.php">Edit your profile</a>
 
 <?php include '../view/partials/footer.php'; ?>
 
