58bac819aae8470450da37f342ee0f9da0a8c1e7
alyssamjia <alyssajia36@gmal.com>
2020-03-25 02:51:28 -0700

    ok so i overhauled the view folder, added a css folder to hold the indivisual css for each php page, also moved </head> <body> from header to each view php page inorder to make each unique css. i finished the signin page unless you want to change something. the index is not finished cause im still looking for a good simple one while i had one already for the signin page. i think thats it any thing i forgot to mention ask

--- app/view/partials/header.php
+++ app/view/partials/header.php
@@ -8,5 +8,6 @@
     integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
     <title>Healthcare System</title>
+    
     <!--<link rel="stylesheet" href="style.css">-->
     
