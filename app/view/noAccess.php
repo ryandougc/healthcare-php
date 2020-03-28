040ad573b6c9e37401edd5791e6378c324eb2a42
ryandougc <ryan.carr@email.kpu.ca>
2020-03-28 12:38:35 -0700

    Fixed: no access page link update

--- app/view/noAccess.php
+++ app/view/noAccess.php
@@ -6,7 +6,7 @@ include '../view/partials/header.php';
 
 <h3>You do not have access to this page!</h3>
 <p>You must be a patient to view this page.</p>
-<a href="/healthcare-php/app/view/homepage.php">Return to Home</a>
+<a href="/healthcare-php/app/view/index.php">Return to Home</a>
 <br/>
 <p>If you believe this was a mistake, please contact an administrator asap.</p>
 
