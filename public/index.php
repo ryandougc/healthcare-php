e4bcec0613b214ed3d2f9b0b317fed1ef7f65ed2
alyssamjia <alyssajia36@gmal.com>
2020-03-29 16:46:25 -0700

    made landing page and fixed the links in href for the landing page

--- public/index.php
+++ public/index.php
@@ -1,5 +1,4 @@
-</head>
-<body>
+<body class="bodysignin">
 
 <?php
 include '../app/view/partials/header.php';
@@ -9,16 +8,22 @@ if(isset($_SESSION['signedin'])){
     header('location: ./app/view/homepage.php');
 }*/
 ?>
+
+
 <div class="container my-4">
-        <div class="border border-light p-3 mb-4">
-                <div class="text-center">
-                        <a href="app/view/signinpage.php">
-                        <button class="btn btn-primary">Sign In</button>
-                        <a href="app/view/signuppage.php">
-                        <button class="btn btn-primary">Sign Up</button>
-                </dev>
-        </dev>
-</dev>
+        <div class="text-center">
+                <h1 class="display-1 mb-3 font-weight-normal">Healthcare</h1>
+                <div class="border border-dark rounded-pill p-4 mb-4" >
+                        <div class="text-center">
+                                <a href="../app/view/signinpage.php">
+                                <button class="btn btn-primary">Sign In</button>
+                                
+                                <a href="../app/view/signuppage.php">
+                                <button class="btn btn-primary">Sign Up</button>
+                        </div>
+                </div>
+        </div>
+</div>
 <?php include '../app/view/partials/footer.php'; ?>
 
 <!-- class="m-5" -->
