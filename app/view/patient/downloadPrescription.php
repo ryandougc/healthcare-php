06c4fd6eb663efde23992322bbee0e41afca60c8
ryandougc <ryan.carr@email.kpu.ca>
2020-03-30 12:30:44 -0700

    Added access control

--- app/view/patient/downloadPrescription.php
+++ app/view/patient/downloadPrescription.php
@@ -1,5 +1,10 @@
 <?php
 
+//Check if a user is logged in. Send them to the signin page if they aren't
+require_once('../../includes/sessionCheck.php');
+//Only give access to patients
+require_once('../../includes/patientAccess.php');
+
 include '../../model/database.class.php';
 include '../../model/account.class.php';
 include '../../model/visit.class.php';
