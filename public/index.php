a96e7d492d136f09768b0e36e0cc87f85d2a65f1
ryandougc <ryan.carr@email.kpu.ca>
2020-03-29 17:11:40 -0700

    Fixed form links

--- public/index.php
+++ public/index.php
@@ -15,10 +15,10 @@ if(isset($_SESSION['signedin'])){
                 <h1 class="display-1 mb-3 font-weight-normal">Healthcare</h1>
                 <div class="border border-dark rounded-pill p-4 mb-4" >
                         <div class="text-center">
-                                <a href="../app/view/signinpage.php">
+                                <a href="app/view/signinpage.php">
                                 <button class="btn btn-primary">Sign In</button>
                                 
-                                <a href="../app/view/signuppage.php">
+                                <a href="app/view/signuppage.php">
                                 <button class="btn btn-primary">Sign Up</button>
                         </div>
                 </div>
