c845d2d03d13b110ede14acfefb6e40341d403fa
alyssamjia <alyssajia36@gmal.com>
2020-03-29 21:13:17 -0700

    added sidebar to editprofile

--- app/view/patient/editPatientProfile.php
+++ app/view/patient/editPatientProfile.php
@@ -12,6 +12,8 @@ include '../../model/account.class.php';
 include '../../model/patient.class.php';
 include '../../controller/patientcontroller.php';
 
+include '../partials/header.php';
+
 //Get the patient profile data
 $patient = new PatientController();
 $patientDetails = $patient->getPatientProfile($_SESSION['loginid']);
@@ -58,33 +60,107 @@ if(isset($_POST['submit'])) {
 
 ?>
 
-<!-- Sign up form -->
-<h2>Update Patient Profile</h2>
-<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-    <label for="phoneNumInput">Phone Number</label>
-    <input id="phoneNumInput" type="text" name="phoneNum" value="<?php echo $patientDetails['PatientPhone']; ?>">
+<div class="wrapper">
+        <!-- Sidebar  -->
+        <nav id="sidebar">
+            <div class="sidebar-header">
+                <h3>Healthcare</h3>
+            </div>
 
-    <label for="addresInput">Address</label>
-    <input id="addresInput" type="text" name="address" value="<?php echo $patientDetails['PatientAddress']; ?>">
+            <ul class="list-unstyled components">
+                <li>
+                    <a href="patientHomePage.php">Home</a>
+                </li>
+                <li class="active">
+                    <a href="viewPatientProfile.php">Profile</a>
+                </li>
+                <li>
+                    <a href="editPatientProfile.php">Change Profile</a>
+                </li>
+                <li>
+                    <a href="">Visits</a>
+                </li>
+                <li>
+                    <a href="">Exams</a>
+                </li>
+                <li>
+                    <a href="">Exam Results</a>
+                </li>
+            </ul>
+        </nav>
 
-    <label for="cityInput">City</label>
-    <input id="cityInput" type="text" name="city">
+        <!-- Page Content  -->
+        <div id="content">
 
-    <label for="provinceInput">Province</label>
-    <input id="provinceInput" type="text" name="province">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <div class="container-fluid">
+                    <button type="button" id="sidebarCollapse" class="btn btn-info">
+                        <i class="fas fa-align-left"></i>
+                        <span>Toggle Sidebar</span>
+                    </button>
+                    <button class="btn btn-dark d-inline-block d-lg-none ml-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <i class="fas fa-align-justify"></i>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="nav navbar-nav ml-auto">
+                        </ul>
+                    </div>
+                </div>
+            </nav>
 
-    <label for="postCodeInput">Postal Code</label>
-    <input id="postCodeInput" type="text" name="postCode">
+            <!-- Sign up form -->
+            <h2>Update Patient Profile</h2>
+            <form class="form-editprofile" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+                <div class="form-row">
+                <label for="phoneNumInput">Phone Number</label>
+                <input id="phoneNumInput" class="form-control" type="text" name="phoneNum" 
+                value="<?php echo $patientDetails['PatientPhone']; ?>">
+                </div>
+                <div class="form-row">
+                <label for="addresInput">Address</label>
+                <input id="addresInput" class="form-control" type="text" name="address" 
+                value="<?php echo $patientDetails['PatientAddress']; ?>">
+                </div>
+                <div class="form-row">
+                <label for="cityInput">City</label>
+                <input id="cityInput" class="form-control" type="text" name="city">
+                </div>
+                <div class="form-row">
+                <label for="provinceInput">Province</label>
+                <input id="provinceInput" class="form-control" type="text" name="province">
+                </div>
+                <div class="form-row">
+                <label for="postCodeInput">Postal Code</label>
+                <input id="postCodeInput" class="form-control" type="text" name="postCode">
 
-    <div>
-        <h4>Receive Email Notifications?</h4>
+                <div>
+                    <h4>Receive Email Notifications?</h4>
 
-        <label for="emailNotifNoRadio">No</label>
-        <input id="emailNotifNoRadio" type="radio" name="emailNotifications"  value="0" <?php if($patientDetails['EmailNotifications'] == 0) {echo 'checked'; }?>>
+                    <label for="emailNotifNoRadio">No</label>
+                    <input id="emailNotifNoRadio" type="radio" name="emailNotifications"  value="0" <?php if($patientDetails['EmailNotifications'] == 0) {echo 'checked'; }?>>
 
-        <label for="emailNotifYesRadio">Yes</label>
-        <input id="emailNotifYesRadio" type="radio" name="emailNotifications"  value="1" <?php if($patientDetails['EmailNotifications'] == 1) {echo 'checked'; }?>>
+                    <label for="emailNotifYesRadio">Yes</label>
+                    <input id="emailNotifYesRadio" type="radio" name="emailNotifications"  value="1" <?php if($patientDetails['EmailNotifications'] == 1) {echo 'checked'; }?>>
+                </div>
+            
+                <button type="submit" name="submit">Update</button>
+            </form>
+        </div>
     </div>
-  
-    <button type="submit" name="submit">Update</button>
-</form>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" 
+    integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" 
+    integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" 
+    integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+
+    <script type="text/javascript">
+        $(document).ready(function () {
+            $('#sidebarCollapse').on('click', function () {
+                $('#sidebar').toggleClass('active');
+            });
+        });
+    </script>
+
+<?php include '../partials/footer.php'; ?>
