1323763e0256d5c5f9eaf79af1d8e79cc4d33b63
alyssamjia <alyssajia36@gmal.com>
2020-03-29 19:48:10 -0700

    the side bar is working in view patient profile ignore the contents im fixing that now

--- app/view/partials/header.php
+++ app/view/partials/header.php
@@ -2,13 +2,20 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" 
     integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
+    <!--fonts-->
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js" 
+    integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ" crossorigin="anonymous"></script>
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js" 
+    integrity="sha384-6OIrr52G08NpOFSZdxxz1xdNSndlD4vdcf/q2myIUVO0VsqaGHJsB0RaBE01VTOY" crossorigin="anonymous"></script>
+
     <title>Healthcare System</title>
     
-    <!--<link rel="stylesheet" href="style.css">-->
     <link rel="stylesheet"  href="/healthcare-php/public/css/style.css">
 </head>
