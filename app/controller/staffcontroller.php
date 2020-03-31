b0261341b07683ff545d58ddf82d12ef6f35b555
theajaydeepbrar <61721623+theajaydeepbrar@users.noreply.github.com>
2020-03-30 18:06:39 -0700

    staff

--- app/controller/staffcontroller.php
+++ app/controller/staffcontroller.php
@@ -2,5 +2,29 @@
 
 class StaffController extends Staff{
 
+    private $Array = []; 
+
+
+    public function getStaffProfile(){
+
+
+        $staff = new staff();
+        $staff->getStaffAccount();
+
+        echo $staff;
+
     
+    }
+
+    public function getOwnProfile(){
+
+        $staffModel = new staff();
+
+        $staffProfile->getOwnProfile(); 
+
+        echo $staffProfile;
+
+
+    }
+
 }
--- app/model/staff.class.php
+++ app/model/staff.class.php
@@ -1,6 +1,32 @@
 <?php
 class staff extends Database{
 
+    private $staffID;
+    private $clinicID;
+
+    public function intializeOwnProfile(){
+
+        //Query
+        $this->$satffID = $tempstaffID;
+        $this->clinicID = $tempClinicID;
+
+    }
+
+    protected function getStaffAccount($accountId) {
+        try{
+            $sql = "SELECT * FROM account WHERE AccountID = ?";
+            $stmt = $this->connect()->prepare($sql);
+            $stmt->execute([$accountId]);
+
+            $results = $stmt->fetch(); 
+            return $results;
+        }
+        catch(PDOException $e){
+            echo "Error in query 'getAccountDetails': " . $e->getMessage();
+            exit();
+        }
+    }
+
     public function staffSearchVists($VisitID){
 
         $sql = "SELECT * FROM VISIT 
